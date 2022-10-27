d3854ea75fe857a61afdf5203baeb378b23b4840
l156521 <1565211920@qq.com>
2022-10-27 21:30:50 +0800

    Mybatis

---
+++ mybatis-demo/src/main/java/com/itheima/mapper/BrandMapper.java
@@ -0,0 +1,13 @@
+package com.itheima.mapper;
+
+
+import com.itheima.pojo.Brand;
+
+import java.util.List;
+
+public interface BrandMapper {
+    /**
+     *
+     */
+    public List<Brand> selectAll();
+}
