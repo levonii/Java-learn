4f592271a5ea96eb80de5dc90027ed1bf0090286
l156521 <1565211920@qq.com>
2022-10-31 09:27:37 +0800

    genericity/src/genericitydemo/TestDemo.java

---
+++ genericity/src/demo2/Animal.java
@@ -0,0 +1,7 @@
+package demo2;
+
+public class Animal {
+    public Animal(){
+        System.out.println("我是动物");
+    }
+}
---
+++ genericity/src/demo2/Dog.java
@@ -0,0 +1,10 @@
+package demo2;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Dog {
+    public Dog(){
+        System.out.println("我是狗");
+    }
+}
+
